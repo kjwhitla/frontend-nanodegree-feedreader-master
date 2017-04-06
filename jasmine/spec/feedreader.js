95637d835b05d562891d2deb8d9fb3e58586b4e4
Keegan Whitla <kjwhitla@gmail.com>
2017-04-05 17:25:39 -0700

    bug: fixed code Udacity review Beautified as well.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,88 +8,88 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* #1 - first test suite - named 'RSS Feeds' */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function () {
 
         /* #1.1 - Note: allFeeds variable has been defined */
-        it('are defined', function() {
+        it('are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
         /* #1.2 - Note: This test ensures a URL is defined and not empty within the allFeeds object */
-        it('each feed has a URL defined', function() {
-            allFeeds.forEach(function(feed) {
+        it('each feed has a URL defined', function () {
+            allFeeds.forEach(function (feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url).not.toBe('');
             });
         });
 
-        
+
         /* #1.3 - Note: This test loop ensures a name is defined in the allFeeds object */
-        it('each feed has a name defined', function() {
-            allFeeds.forEach(function(feed) {
-                 expect(feed.name).toBeDefined();
-                 expect(feed.name).not.toBe('');
+        it('each feed has a name defined', function () {
+            allFeeds.forEach(function (feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
             });
         });
-        
+
     });
 
     /*#2 - second test suite - named "The menu" */
-        describe('The menu', function() {
-            
+    describe('The menu', function () {
+
         /* #2.1 - Note: test ensures the element is hidden by default */
-        it('is hidden by default', function() {
+        it('is hidden by default', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
-        }); 
-            
-        /* #2.2 - Note: test ensures the menu changes visibility when clicked */
-         it('changes visibility when the menu icon is clicked', function() {
-             $('.menu-icon-link').trigger('click');
-             expect($('body').hasClass('menu-hidden')).toBe(false);
-             $('.menu-icon-link').trigger('click');
-             expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
         });
+
+        /* #2.2 - Note: test ensures the menu changes visibility when clicked */
+        it('changes visibility when the menu icon is clicked', function () {
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
 
     /* #3 - third test suite - named "Initial Entries" */
-    describe('Initial Entries', function() {
+    describe('Initial Entries', function () {
 
 
-        /* #3.1 - Note:  This test ensures when the loadFeed is called that there is at least one .entry element in the feed */ 
-        beforeEach(function(done) {
-            loadFeed(0, function() {
-                done(); 
+        /* #3.1 - Note:  This test ensures when the loadFeed is called that there is at least one .entry element in the feed */
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                done();
             });
         });
-        
-        it('has at least one entry in the feed container', function(done) {
-            expect($('.feed .entry').children().length).toBeGreaterThan(0);
+
+        it('has at least one entry in the feed container', function (done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
         });
     });
 
     /* #4 - fourth test suite:  - named "New Feed Selection" */
-     describe('New Feed Selection', function() {
+    describe('New Feed Selection', function () {
 
-         /* #4.1 - Note: This test ensures when a new feed is loaded the content actually changes */
-         var content, 
-            updatedContent; 
+        /* #4.1 - Note: This test ensures when a new feed is loaded the content actually changes */
+        var content,
+            updatedContent;
 
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach(function (done) {
+            loadFeed(1, function () {
                 content = $('.feed').html();
                 done();
             });
         });
 
-        it('changes content when a new feed is loaded', function(done) {
-            loadFeed(1, function() {
+        it('changes content when a new feed is loaded', function (done) {
+            loadFeed(1, function () {
                 updatedContent = $('.feed').html()
                 expect(updatedContent).not.toBe(content);
             });
         });
-         
-     });
+
+    });
 }());
